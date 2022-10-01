f7ad94517788afc1b68e24a01125314742c5c494
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-10-01 18:25:29 -0500

    modules

---
+++ modules/fs.js
@@ -0,0 +1,42 @@
+//const moduleName =require('fs');
+const fs =require('fs');
+
+function read(path, cb){
+    fs.readFile(path, (err, data) => {
+        //The file has already been read
+        // if(err){
+        //     cb(err);
+        // } else {
+        //     cb(null, data);
+        // }
+        cb(data.toString());
+    });
+}
+
+
+function write(path, content, cb){
+    fs.writeFile(path, content, function(err){
+        if(err){
+            cb( 'fs.writeFile error' + err);
+        } else {
+            cb('fs.writeFile success');
+        }
+    });
+}
+
+
+
+function erase(path, cb){
+    fs.unlink(path, function(err){
+        if(err){
+            cb('fs.unlink error: ' + err);
+        } else {
+            cb('fs.unlink success');
+        }
+    });
+}
+
+
+//read(__dirname + '/file.txt', console.log)
+//write(__dirname + '/newFile.txt', 'Hello files world', console.log)
+erase(__dirname + '/newFile.txt', console.log)
---
+++ modules/globals.js
@@ -0,0 +1,29 @@
+//console.log(global);
+//console.log(process);
+//console.log(__dirname);
+//console.log(__filename);
+
+
+// let i = 0;
+// let interval = setInterval( function() {
+//     console.log('Hello');
+//     if (i === 3) {
+//         clearInterval(interval);
+//     }
+//     i++;
+// }, 1000 );
+
+
+// setImmediate( function() { 
+//     console.log('Hi');
+// })
+
+
+
+// --- GLOBAL VARIABLES ---
+global.myVar = 'My var';
+console.log(myVar);
+
+
+
+
